8a9c0d8f4daf6b6d8d034e4e1c0b1ef0ed0ca67d
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-09-16 01:34:57 +0300

    restore main changes

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/core/routing/app_router.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routing/routes.dart';
+import 'core/theming/colors.dart';
 
 class DocApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -7,6 +11,19 @@ class DocApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Doc App',
+          theme: ThemeData(
+            primaryColor: ColorsManager.mainBlue,
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          initialRoute: Routes.onBoardingScreen,
+          onGenerateRoute: appRouter.onGenerateRoute,
+        ),
+    );
   }
 }
