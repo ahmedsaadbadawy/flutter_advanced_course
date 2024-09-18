378b61d8ebb50570a6fae696c679de72f00af5f2
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-09-19 02:40:00 +0300

    - onboarding screen done - added images & svgs folders for assets. - added text styles

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -5,10 +5,12 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('LoginScreen'),
+        ),
+        body: const Center(
+          child: Text('LoginScreen is working'),
+        ));
   }
 }
