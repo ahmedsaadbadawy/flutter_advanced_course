d6b4d312a8febf048b87c688e8c8e710e573b945
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-09-27 03:47:22 +0300

    - Added Sign up screen ui _ Sign up screen api integration done

--- lib/features/signup/ui/widgets/already_have_account_text.dart
+++ lib/features/signup/ui/widgets/already_have_account_text.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/core/helpers/extensions.dart';
+
 import '../../../../core/routing/routes.dart';
 import '../../../../core/theming/styles.dart';
 
-class DontHaveAccountText extends StatelessWidget {
-  const DontHaveAccountText({super.key});
+class AlreadyHaveAccountText extends StatelessWidget {
+  const AlreadyHaveAccountText({super.key});
+
   @override
   Widget build(BuildContext context) {
     return RichText(
@@ -13,15 +15,15 @@ class DontHaveAccountText extends StatelessWidget {
       text: TextSpan(
         children: [
           TextSpan(
-            text: 'Don\'t have an account?',
+            text: 'Already have an account?',
             style: TextStyles.font13DarkBlueRegular,
           ),
           TextSpan(
-            text: ' Sign Up',
+            text: ' Login',
             style: TextStyles.font13BlueSemiBold,
             recognizer: TapGestureRecognizer()
               ..onTap = () {
-                context.pushReplacementNamed(Routes.signUpScreen);
+                context.pushReplacementNamed(Routes.loginScreen);
               },
           ),
         ],
